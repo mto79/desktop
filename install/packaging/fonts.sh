22b8e41d4b76b3ff434605e6ff0b06d4b16aa374
MTO79 <marc@mto.nu>
2025-12-09 18:34:00 -0500

    Starship, JetBrains mono, github-cli

--- install/packaging/fonts.sh
+++ install/packaging/fonts.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/.local/share/fonts
-fc-cache
+cd /tmp 
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip -o JetBrainsMono.zip -d JetBrainsMono 
+cp JetBrainsMono/*.ttf ~/.local/share/fonts/
+fc-cache -fv
--- install/preflight/dnf.sh
+++ install/preflight/dnf.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-# COPR for USWM and Hyprland packages
-sudo dnf copr enable -y solopasha/hyprland
+echo "Enabling COPR Repos"
 
-# COPR for mise
-sudo dnf copr enable -y jdxcode/mise
+# Array of COPR repos to enable
+COPR_REPOS=(
+  "solopasha/hyprland" # COPR for USWM and Hyprland packages
+  "jdxcode/mise"       # COPR for mise
+  "atim/starship"      # COPR for starship
+  # "scottames/ghostty"    # COPR for Ghostty
+)
+
+# Loop through array and enable each repo
+for repo in "${COPR_REPOS[@]}"; do
+  echo "Enabling COPR repo: $repo"
+  sudo dnf copr enable -y "$repo"
+done
