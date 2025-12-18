8c45bbbe509348987b9af83d086e82883e05d245
MTO79 <marc@mto.nu>
2025-12-18 13:55:12 +0100

    no envs

--- config/fish/conf.d/envs.fish
+++ config/fish/conf.d/envs.fish
@@ -5,20 +5,6 @@ set -gx BAT_THEME ansi
 set -g fish_greeting
 set -gx KITTY_WINDOW_ID 1
 
-## Terraform/Opentofu
-### DigitalOcean
-# set -x TF_VAR_digitalocean_api_token dop_xxxxxxx
-### Pve
-set -x TF_VAR_pve_api_url https://mto-pve.mto.lan:8006/
-set -x TF_VAR_pve_api_token terraform@pve!provider=295520fc-9571-4b4e-b0b0-17ad7b6962b1
-
-## Git
-# set -g -x GITLAB_ACCESS_TOKEN glpat-xxxxxxxxxxxxx
-
-## Scans
-set -x SCANS_DIR ~/Data/MTO/Documents/Scans
-set -x SCANS_TMP_DIR ~/Data/MTO/Documents/Scans/tmp
-
 ## Tmux
 set -x TMUX_SESSIONIZER_CONFIG_FILE ~/.tmux/configs/tmux-sessionizer
 
