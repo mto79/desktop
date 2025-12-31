87c45778e9df02ed36819e5423b8116b39e60095
MTO79 <marc@mto.nu>
2025-12-31 15:02:56 +0100

    nvim: open snacks explorer by default

---
+++ config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		-- only when starting nvim without files
+		if vim.fn.argc() == 0 then
+			require("snacks").explorer()
+		end
+	end,
+})
