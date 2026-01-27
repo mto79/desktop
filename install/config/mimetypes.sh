f826b322caaaa48eea7ec445ed9e5ac60e2eeed2
Marc Ouwerkerk <marc.ouwerkerk@nationaalarchief.nl>
2026-01-28 00:42:33 +0100

    mimetypes, update scripts, keyring, wiremix,

--- install/config/mimetypes.sh
+++ install/config/mimetypes.sh
@@ -14,22 +14,15 @@ xdg-mime default imv.desktop image/tiff
 # Open PDFs with the Document Viewer
 xdg-mime default org.gnome.Evince.desktop application/pdf
 
-## Browsers
-
-# Firefox → firefox.desktop
-# Chromium → chromium.desktop
-# Brave → brave-browser.desktop
-# Chrome → google-chrome.desktop
+# Use Brave as the default browser
+xdg-settings set default-web-browser brave-browser.desktop
+xdg-mime default brave-browser.desktop x-scheme-handler/http
+xdg-mime default brave-browser.desktop x-scheme-handler/https
 
 # Use Chromium as the default browser
 # xdg-settings set default-web-browser chromium-browser.desktop
 # xdg-mime default chromium-browser.desktop x-scheme-handler/http
 # xdg-mime default chromium-browser.desktop x-scheme-handler/https
-
-# Use Brave as the default browser
-xdg-settings set default-web-browser brave-browser.desktop
-xdg-mime default brave-browser.desktop x-scheme-handler/http
-xdg-mime default brave-browser.desktop x-scheme-handler/https
 
 # Open video files with mpv
 xdg-mime default mpv.desktop video/mp4
@@ -47,3 +40,22 @@ xdg-mime default mpv.desktop video/x-ms-asf
 xdg-mime default mpv.desktop video/x-ogm+ogg
 xdg-mime default mpv.desktop video/x-theora+ogg
 xdg-mime default mpv.desktop application/ogg
+
+# Open text files with nvim
+xdg-mime default nvim.desktop text/plain
+xdg-mime default nvim.desktop text/english
+xdg-mime default nvim.desktop text/x-makefile
+xdg-mime default nvim.desktop text/x-c++hdr
+xdg-mime default nvim.desktop text/x-c++src
+xdg-mime default nvim.desktop text/x-chdr
+xdg-mime default nvim.desktop text/x-csrc
+xdg-mime default nvim.desktop text/x-java
+xdg-mime default nvim.desktop text/x-moc
+xdg-mime default nvim.desktop text/x-pascal
+xdg-mime default nvim.desktop text/x-tcl
+xdg-mime default nvim.desktop text/x-tex
+xdg-mime default nvim.desktop application/x-shellscript
+xdg-mime default nvim.desktop text/x-c
+xdg-mime default nvim.desktop text/x-c++
+xdg-mime default nvim.desktop application/xml
+xdg-mime default nvim.desktop text/xml
--- install/login/all.sh
+++ install/login/all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+source "$DESKTOP_INSTALL/login/default-keyring.sh"
 source "$DESKTOP_INSTALL/login/systemd.sh"
 source "$DESKTOP_INSTALL/login/plymouth.sh"
 source "$DESKTOP_INSTALL/login/sddm.sh"
