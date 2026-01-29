b667871b90783af1e2c197211043c03d8de32c1d
Marc Ouwerkerk <marc.ouwerkerk@nationaalarchief.nl>
2026-01-30 00:35:04 +0100

    tz-select and webapp

---
+++ install/packaging/webapps.sh
@@ -0,0 +1,3 @@
+desktop-webapp-install "Google Maps" https://maps.google.com "Google Maps.png"
+desktop-webapp-install "ChatGPT" https://chatgpt.com/ ChatGPT.png
+desktop-webapp-install "YouTube" https://youtube.com/ YouTube.png
