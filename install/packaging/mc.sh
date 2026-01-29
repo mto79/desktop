dab53d50e5f011a8bfe3e4d44e415fb4d633abc6
Marc Ouwerkerk <marc.ouwerkerk@nationaalarchief.nl>
2026-01-29 16:55:49 +0100

    added minio client

---
+++ install/packaging/mc.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if command -v mc &>/dev/null; then
+  echo "mc is already installed. Current version:"
+  mc --help
+else
+  echo "Installing mc (Minio Client)"
+
+  # Download oc
+  curl -LO "https://dl.min.io/client/mc/release/linux-amd64/mc"
+
+  # Move to /usr/local/bin
+  sudo mv mc /usr/local/bin/
+
+  # Make executable
+  sudo chmod +x /usr/local/bin/mc
+
+  # Verify installation
+  mc --help
+fi
--- install/packaging/oc.sh
+++ install/packaging/oc.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-if command -v oc &> /dev/null; then
-    echo "oc is already installed. Current version:"
-    oc version --client
+if command -v oc &>/dev/null; then
+  echo "oc is already installed. Current version:"
+  oc version --client
 else
-    echo "Installing oc (Openshift Client)"
+  echo "Installing oc (Openshift Client)"
 
-    # Download oc
-    curl -LO "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz"
+  # Download oc
+  curl -LO "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz"
 
-    # Extract the tarball
-    tar -xvzf openshift-client-linux.tar.gz
+  # Extract the tarball
+  tar -xvzf openshift-client-linux.tar.gz
 
-    # Move to /usr/local/bin
-    sudo mv oc /usr/local/bin/
+  # Move to /usr/local/bin
+  sudo mv oc /usr/local/bin/
 
-    # Make executable
-    sudo chmod +x /usr/local/bin/oc
+  # Make executable
+  sudo chmod +x /usr/local/bin/oc
 
-    # Verify installation
-    oc version --client
+  # Verify installation
+  oc version --client
 fi
