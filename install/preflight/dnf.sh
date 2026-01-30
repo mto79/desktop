9718f1c3919e1318bfeb5dad56035be551a74584
Marc Ouwerkerk <marc.ouwerkerk@nationaalarchief.nl>
2026-01-30 12:13:35 +0100

    dnf -y flag missing

--- install/preflight/dnf.sh
+++ install/preflight/dnf.sh
@@ -2,5 +2,5 @@
 
 echo "Enabling DNF Repos"
 
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf makecache
