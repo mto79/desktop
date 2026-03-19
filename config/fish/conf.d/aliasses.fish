c5fc9530d15376a15463f1432a0f26610c680945
Marc Ouwerkerk <marc.ouwerkerk@nationaalarchief.nl>
2026-03-19 23:12:31 +0100

    fish aliasses added

--- config/fish/conf.d/aliasses.fish
+++ config/fish/conf.d/aliasses.fish
@@ -19,7 +19,16 @@ abbr -a v nvim
 abbr -a vi nvim
 abbr -a vim nvim
 
-## Custom
+## AI
+abbr -a c opencode
+abbr -a cx 'printf "\033[2J\033[3J\033[H" && claude --allow-dangerously-skip-permissions'
+
+## Container
+abbr -a d docker
+abbr -a p podman
+
+## Tmux
+abbr -a t 'tmux attach || tmux new -s Work'
 
 ## Lazygit
 abbr -a lg lazygit
